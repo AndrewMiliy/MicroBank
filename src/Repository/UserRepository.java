9fbb823f77341feb9f459463ed2b7df5a3a49029
Kirrrra <kirrrra007@gmail.com>
2023-11-27 21:05:09 +0100

    update

--- src/Repository/UserRepository.java
+++ src/Repository/UserRepository.java
@@ -1,4 +1,26 @@
 package Repository;
 
+import Model.UserModel;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class UserRepository {
+    private Map<String, UserModel> users = new HashMap<>();
+
+    public void addUser(String userEmail, UserModel user) {
+        users.put(user.getId(), user);
+    }
+
+    public UserModel getUserById(String userId) {
+        return users.get(userId);
+    }
+
+    public void deleteAccount(String userId, UserModel user) {
+        users.put(userId, user);
+    }
+
+    public void updateUser(String userId, UserModel user) {
+        users.put(userId, user);
+    }
 }
