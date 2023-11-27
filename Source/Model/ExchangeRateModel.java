58e8eac19e8f61b5c515934b2e4a2f15b7f90467
ANDREW\Kashiwa <Kashiwa.akio@gmail.com>
2023-11-27 19:09:59 +0100

    + Add ExchangeRateModel code

--- Source/Model/ExchangeRateModel.java
+++ Source/Model/ExchangeRateModel.java
@@ -1,4 +1,47 @@
 package Model;
 
 public class ExchangeRateModel {
+    private String currencyFrom; // Исходная валюта
+    private String currencyTo;   // Целевая валюта
+    private double rate;         // Курс обмена
+
+    // Конструкторы, геттеры и сеттеры
+    public ExchangeRateModel(String currencyFrom, String currencyTo, double rate) {
+        this.currencyFrom = currencyFrom;
+        this.currencyTo = currencyTo;
+        this.rate = rate;
+    }
+
+    public String getCurrencyFrom() {
+        return currencyFrom;
+    }
+
+    public void setCurrencyFrom(String currencyFrom) {
+        this.currencyFrom = currencyFrom;
+    }
+
+    public String getCurrencyTo() {
+        return currencyTo;
+    }
+
+    public void setCurrencyTo(String currencyTo) {
+        this.currencyTo = currencyTo;
+    }
+
+    public double getRate() {
+        return rate;
+    }
+
+    public void setRate(double rate) {
+        this.rate = rate;
+    }
+
+    @Override
+    public String toString() {
+        return "ExchangeRateModel{" +
+                "currencyFrom='" + currencyFrom + '\'' +
+                ", currencyTo='" + currencyTo + '\'' +
+                ", rate=" + rate +
+                '}';
+    }
 }
