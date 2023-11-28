b521c4b5cea92252fb2653dd0d74424ef88a4225
Kirrrra <kirrrra007@gmail.com>
2023-11-28 19:10:32 +0100

    Currency Service

--- src/Service/CurrencyService.java
+++ src/Service/CurrencyService.java
@@ -1,4 +1,39 @@
 package Service;
 
+import Model.CurrencyModel;
+import Model.UserModel;
+import Model.UserRole;
+import Repository.CurrencyRepository;
+
 public class CurrencyService {
+    private CurrencyRepository cR;
+
+    public CurrencyService(CurrencyRepository cR) {
+        this.cR = cR;
+    }
+    public void addCurrency(UserModel admin, CurrencyModel currency) {
+        if (admin.getUserRole().equals(UserRole.ADMIN)) {
+            cR.addCurrency(currency);
+        } else {
+            System.out.println("Только администратор может добавить новую валюту.");
+        }
+    }
+    public void deleteCurrency(UserModel admin, String code) {
+        if (admin.getUserRole().equals(UserRole.ADMIN)) {
+            cR.deleteCurrency(code);
+        } else {
+            System.out.println("Только администратор может удалить валюту.");
+        }
+    }
+    public void updateCurrency(UserModel admin, String code, CurrencyModel currency) {
+        if (admin.getUserRole().equals(UserRole.ADMIN)) {
+            cR.updateCurrency(code, currency);
+        } else {
+            System.out.println("Только администратор может изменять валюту.");
+        }
+    }
+
+
+
+
 }
