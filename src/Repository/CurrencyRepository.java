816d63a797085ca1c4c698d8dc2980e8141a4a60
ANDREW\Kashiwa <Kashiwa.akio@gmail.com>
2023-11-28 17:46:02 +0100

    + Currency Repository

--- src/Repository/CurrencyRepository.java
+++ src/Repository/CurrencyRepository.java
@@ -1,4 +1,26 @@
 package Repository;
 
+import Model.CurrencyModel;
+
+import java.util.Map;
+import java.util.TreeMap;
+
 public class CurrencyRepository {
+    private Map<String, CurrencyModel> currencies = new TreeMap<>();
+
+    public void addCurrency(CurrencyModel currency) {
+        currencies.put(currency.getCodeName(), currency);
+    }
+
+    public CurrencyModel getCurrency(String code) {
+        return currencies.get(code);
+    }
+
+    public void updateCurrency(String code, CurrencyModel currency) {
+        currencies.put(code, currency);
+    }
+
+    public void deleteCurrency(String code) {
+        currencies.remove(code);
+    }
 }
